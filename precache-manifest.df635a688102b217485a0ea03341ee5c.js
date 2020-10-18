344cc54dc5536267b695ec41cacb9e194951466b
olegsirivlya <o.sirivlya@gmail.com>
2020-10-18 14:59:54 +0300

    Updates

---
+++ precache-manifest.df635a688102b217485a0ea03341ee5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5bb1a6829b65f8adcb66ee4f5967e9",
+    "url": "/Binary-Vision-Studios/moken-website/index.html"
+  },
+  {
+    "revision": "07bbd6593909308b8fa7",
+    "url": "/Binary-Vision-Studios/moken-website/static/css/main.61e17455.chunk.css"
+  },
+  {
+    "revision": "07fba6eefed6e81d7e05",
+    "url": "/Binary-Vision-Studios/moken-website/static/js/2.c9ecd394.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Binary-Vision-Studios/moken-website/static/js/2.c9ecd394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bbd6593909308b8fa7",
+    "url": "/Binary-Vision-Studios/moken-website/static/js/main.7345463a.chunk.js"
+  },
+  {
+    "revision": "a038632da8ec4cf6dc47",
+    "url": "/Binary-Vision-Studios/moken-website/static/js/runtime-main.3fbf2f83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Binary-Vision-Studios/moken-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
