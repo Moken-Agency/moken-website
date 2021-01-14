0a535e98d1c9edd6ae2d76dbc20df30813b9bb73
olegsirivlya <o.sirivlya@gmail.com>
2021-01-14 23:32:50 +0200

    quick fix

--- src/components/Header/header-options.js
+++ src/components/Header/header-options.js
@@ -68,7 +68,7 @@ export default {
       dividerColor: "#ffc4af",
       subRouts: [
         {
-          title: "AboutMoken agency",
+          title: "About Moken agency",
           route: "/about-agency",
         },
         {
